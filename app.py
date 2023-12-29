12e7446a9b2bc194965c0c32986ce05120ade066
Paul Adrian <pauladrian0224@gmail.com>
2023-12-29 15:08:54 +0800

    error

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 13:58:02 2023
+
+@author: henry
+"""
+
+
+
 from flask import Flask, render_template, Response, request
 import json
 import argparse
